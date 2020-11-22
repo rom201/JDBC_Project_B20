11da8117562eaf47df06bf9dd6a64397d5528784
rom201 <kovalchukr1104@gmail.com>
2020-11-22 16:59:07 -0500

    moving pointer

--- src/test/java/day1/LoopingResultSet.java
+++ src/test/java/day1/LoopingResultSet.java
@@ -14,6 +14,28 @@ public class LoopingResultSet {
         ResultSet rs = stmnt.executeQuery("SELECT * FROM REGIONS");
 
 
+        // this method return boolean value
+//        while (rs.next()){
+  //          System.out.println("Region_ID : " + rs.getString("REGION_ID"));
+    //        System.out.println("Region_Name : " + rs.getString("REGION_NAME"));
+      //  }
+
+        //location get all country
+        rs = stmnt.executeQuery("Select * from countries");
+         //rs.next();
+         //System.out.println(rs.getString(1));
+        while (rs.next()){
+            System.out.println(rs.getString(1)+" "+
+                    rs.getString(2)+" "+
+                    rs.getString(3)
+            );
+        }
+
+
+
+
+
+
 
 
 
---
+++ src/test/java/day1/MovingResultSetPointer.java
@@ -0,0 +1,57 @@
+package day1;
+
+import java.sql.*;
+
+public class MovingResultSetPointer {
+
+
+    // we want to create a statement object that generate
+    // ResultSet that can move forward and backward anytime
+    public static void main(String[] args) throws SQLException {
+        // REPLACE THIS IP ADDRESS WITH YOUR OWN THAT WORKING IN SQL DEVELOPER
+        String connectionStr = "jdbc:oracle:thin:@54.236.21.55:1521:XE";
+        String username = "hr";
+        String password = "hr";
+        Connection conn = DriverManager.getConnection(connectionStr, username, password);
+
+        Statement stmnt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+
+        ResultSet rs = stmnt.executeQuery("SELECT * FROM REGIONS");
+
+        while (rs.next()){
+            System.out.println("Region_NAME "+rs.getString("REGION_NAME"));
+        }
+
+        //rs.previous();
+        //System.out.println("Region_NAME "+rs.getString("REGION_NAME"));
+
+        while (rs.previous()){
+            System.out.println("Region_NAME "+rs.getString("REGION_NAME"));
+        }
+
+
+        rs.beforeFirst();// before
+        rs.first();// first row
+        rs.last();// last
+        rs.afterLast();// after last
+        rs.absolute(3);// move to cpesific
+
+        rs.last();
+        int currentRowNum = rs.getRow();
+        System.out.println("Row Count ="+currentRowNum);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
