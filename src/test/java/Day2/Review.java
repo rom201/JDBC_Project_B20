1ca33adb28ffbfcde14a64607a419bf76a69be6b
rom201 <kovalchukr1104@gmail.com>
2020-11-24 19:46:20 -0500

    second commot day2 jdbc

--- src/test/java/Day2/Review.java
+++ src/test/java/Day2/Review.java
@@ -18,8 +18,36 @@ public class Review {
 
         rs.first();// or
         System.out.println("First column value in Jobs " +rs.getString(1));
-        System.out.println("First column value in Jobs " +rs.getString(2));
+        System.out.println("Second column value in Jobs " +rs.getString(2));
 
+// move to row n 7
+        rs.absolute(7);
+        System.out.println("First column value in Jobs in row 7 " +rs.getString(1));
+        System.out.println("Second column value in Jobs in row 7 " +rs.getString(2));
+
+
+        // move to last n
+        rs.last();
+        System.out.println("First column value in Jobs in row last " +rs.getString(1));
+        System.out.println("Second column value in Jobs in row last " +rs.getString(2));
+
+// move to previos
+        rs.previous();
+        System.out.println("First column value in Jobs in row before row last " +rs.getString(1));
+        System.out.println("Second column value in Jobs in row before lastrow last " +rs.getString(2));
+
+        System.out.println("-----------------------------------loop from top to bottom - JOB_ID");
+
+        rs.beforeFirst();
+        while (rs.next()){
+            System.out.println("loop first column  "+ rs.getString("JOB_ID"));
+        }
+
+        System.out.println("\n----------------loop from ---- min salary as number");
+        rs.afterLast();
+        while ((rs.previous())){
+            System.out.println("Min salary column as number " + rs.getDouble("min_salary"));
+        }
 
 
 
